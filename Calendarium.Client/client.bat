803a3bee61c2c71d141b99c9d9fcda92bd27ba24
bernd <bernd@8c953974-d27b-604b-8fe8-e27cebbc196c>
2007-03-12 19:52:29 +0000

    initiales Buildfile mit Deployment für den Client
    git-svn-id: https://swtsvn.w-hs.de/svn/swtcal@10 8c953974-d27b-604b-8fe8-e27cebbc196c

--- Calendarium.Client/client.bat
+++ Calendarium.Client/client.bat
@@ -1,10 +1,7 @@
 call init.bat
 
 set SWING_HOME=%JAVA_JRE%\lib
-
 set JCP=%JAVA_JRE%\lib\rt.jar;%SWING_HOME%\swingall.jar;%CALENDARIUM_HOME%; 
 set JAVA_BIN=%JAVA_JRE%\bin\java
-if exist %JAVA_HOME%\bin\jre.exe set JAVA_BIN=%JAVA_HOME%\bin\jre
 
-set CLASSPATH=.
-java -Djava.security.policy=java.policy client.Client
+%JAVA_BIN% -cp %JCP% -Djava.security.policy=java.policy client.Client
