15f100eae454ef56626c794efe469288bb154285
bernd <bernd@8c953974-d27b-604b-8fe8-e27cebbc196c>
2007-03-13 15:46:19 +0000

    Javadoc-Kommentare und Formatierung geaendert.
    git-svn-id: https://swtsvn.w-hs.de/svn/swtcal@19 8c953974-d27b-604b-8fe8-e27cebbc196c

--- Calendarium.Base/src/interfaces/TerminSetInterface.java
+++ Calendarium.Base/src/interfaces/TerminSetInterface.java
@@ -12,36 +12,100 @@ import basisklassen.Termin;
 
 public interface TerminSetInterface extends Remote
 {
-    // Termine an einem Tag ausgeben
+    /** Termine an einem Tag ausgeben
+     * 
+     * @param vomDat
+     * @param personenListe
+     * @return
+     * @throws RemoteException
+     */
     public Vector getTermineVom(Datum vomDat, Vector personenListe) throws RemoteException;
 
-	// Termine in einem Zeitraum ausgeben
+	/** Termine in einem Zeitraum ausgeben
+	 * 
+	 * @param vonDat
+	 * @param bisDat
+	 * @param personenListe
+	 * @return
+	 * @throws RemoteException
+	 */
 	public Vector[] getTermineVonBis(Datum vonDat, Datum bisDat, Vector personenListe) throws RemoteException;
 
-    // Personen mit Terminen
+    /** Personen mit Terminen
+     * 
+     * @param vonDat
+     * @param bisDat
+     * @param personenListe
+     * @return
+     * @throws RemoteException
+     */
     public Vector[] getPersonsWithTermin(Datum vonDat, Datum bisDat, Vector personenListe) throws RemoteException;
 
-	// einen bestimmten Termin ausgeben
+	/** einen bestimmten Termin ausgeben
+	 * 
+	 * @param id
+	 * @return
+	 * @throws RemoteException
+	 */
 	public Termin getTerminByID(long id) throws RemoteException;
 
-    // Konflikte ausgeben
+    /** Konflikte ausgeben
+     * 
+     * @param termin
+     * @return
+     * @throws RemoteException
+     */
     public Vector getKonflikte(Termin termin) throws RemoteException;
 
-    // konfliktfreie Intervalle ausgeben
+    /** konfliktfreie Intervalle ausgeben
+     * 
+     * @param persons
+     * @param bgn
+     * @param end
+     * @param wk
+     * @return
+     * @throws RemoteException
+     */
     public Vector getFreeOfKonflikte(Hashtable persons, Datum bgn, Datum end, boolean wk) throws RemoteException;
 
-    // Daten säubern
+    /** Daten säubern
+     * 
+     * @param person
+     * @param bis
+     * @throws RemoteException
+     */
     public void deleteUntilDate(Person person, Datum bis) throws RemoteException;
 
-	// Termin anlegen
+	/** Termin anlegen
+	 * 
+	 * @param kuerzel
+	 * @param termin
+	 * @throws RemoteException
+	 */
 	public void create(String kuerzel,Termin termin) throws RemoteException;
 
-	// Termin löschen
+	/** Termin löschen
+	 * 
+	 * @param kuerzel
+	 * @param termin
+	 * @param serie
+	 * @throws RemoteException
+	 */
 	public void delete(String kuerzel,Termin termin, boolean serie) throws RemoteException;
 
-	// Termin ändern
+	/** Termin ändern
+	 * 
+	 * @param kuerzel
+	 * @param termin
+	 * @throws RemoteException
+	 */
 	public void update(String kuerzel,Termin termin) throws RemoteException;
 
-	// Fehlendes Eintragsrecht
+	/** Fehlendes Eintragsrecht
+	 * 
+	 * @param termin
+	 * @param persons
+	 * @throws RemoteException
+	 */
 	public void sendMissingRight(Termin termin, Vector persons) throws RemoteException;
 }
