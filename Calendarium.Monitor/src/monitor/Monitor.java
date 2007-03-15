67837b2fc6020c3e8250284967d3450cf3ab44e0
bernd <bernd@8c953974-d27b-604b-8fe8-e27cebbc196c>
2007-03-15 13:23:27 +0000

    Umbenennung wieder rückgängig gemacht.
    git-svn-id: https://swtsvn.w-hs.de/svn/swtcal@24 8c953974-d27b-604b-8fe8-e27cebbc196c

--- Calendarium.Monitor/src/monitor/Monitor.java
+++ Calendarium.Monitor/src/monitor/Monitor.java
@@ -1,7 +1,7 @@
 package monitor; //
 ///////////////////
 
-import interfaces.DBInterface;
+import interfaces.DBSetInterface;
 import interfaces.MessageServerInterface;
 
 import java.awt.BorderLayout;
@@ -66,7 +66,7 @@ public class Monitor extends JFrame implements ActionListener
     private Hashtable frames = new Hashtable();
     
     //to create and delete tables
-    DBInterface dbi;
+    DBSetInterface dbi;
     
     //boolean if db server was started or not
     private boolean dbserver=false;
@@ -103,7 +103,7 @@ public class Monitor extends JFrame implements ActionListener
             
             
             try {
-            	dbi = (DBInterface) Naming.lookup(hostname + "DBSetRemote");
+            	dbi = (DBSetInterface) Naming.lookup(hostname + "DBSetRemote");
             	//hier kommt man nur her wenn man das DBSetRemote Objekt gefunden hat
             	//das ist nur der fall wenn der DB Server gestartet wurde
 				dbserver=true;
