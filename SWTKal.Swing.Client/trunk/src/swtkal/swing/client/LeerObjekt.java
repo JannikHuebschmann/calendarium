1247480c4cd547e05a2cc14c849ed9d987e896d4
bernd <bernd@8c953974-d27b-604b-8fe8-e27cebbc196c>
2007-09-06 07:00:13 +0000

    Initial import.
    git-svn-id: https://swtsvn.w-hs.de/svn/swtcal@139 8c953974-d27b-604b-8fe8-e27cebbc196c

---
+++ SWTKal.Swing.Client/trunk/src/swtkal/swing/client/LeerObjekt.java
@@ -0,0 +1,23 @@
+package swtkal.swing.client;
+
+// TODO Achtung: im Wesentlichen unveraendert aus Calendarium uebernommen
+
+import swtkal.domain.Datum;
+import swtkal.swing.elements.PanelMitPopup;
+
+//////////////////////////////////////////////////////////////////////////////////////
+// LeerObjekt // LeerObjekt // LeerObjekt // LeerObjekt // LeerObjekt // LeerObjekt //
+//////////////////////////////////////////////////////////////////////////////////////
+public class LeerObjekt extends PanelMitPopup
+{
+	private static final long serialVersionUID = -867792269359339471L;
+	private Datum date = null;
+
+	void setDate(Datum d) {
+		date = d;
+	}
+
+	Datum getDate() {
+		return date;
+	}
+}
